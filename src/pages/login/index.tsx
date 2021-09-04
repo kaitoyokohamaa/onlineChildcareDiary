9f6865401d50be300482e6d830db2a1515dd054f
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-04 11:48:06 +0900

    fix:path fix

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pages } from "src/components/login/Pages";
+import { Pages } from "/src/components/login/Pages";
 export default function Logins() {
   return <Pages />;
 }
--- src/pages/school/index.tsx
+++ src/pages/school/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Layout } from "src/components/common/layout";
-import { Pages } from "src/components/school/Pages";
+import { Layout } from "/src/components/common/layout";
+import { Pages } from "/src/components/school/Pages";
 export default function School() {
   return (
     <Layout>
