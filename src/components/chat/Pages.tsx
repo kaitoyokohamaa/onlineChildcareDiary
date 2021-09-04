00d95e215d97c112ea77836a6d8ebe5096d7c27d
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-04 16:00:26 +0900

    feat:チャットデザインの作成

--- src/components/chat/Pages.tsx
+++ src/components/chat/Pages.tsx
@@ -4,6 +4,8 @@ import { Box, Flex } from "@chakra-ui/layout";
 import { ChatSidebar } from "@/components/chat/chatSidebar";
 import { ChatHeader } from "@/components/chat/chatHeader";
 import { Chat } from "@/components/chat/chat";
+import { ChatForm } from "@/components/chat/chatForm";
+
 export const Pages: VFC = () => {
   return (
     <Box p={0}>
@@ -12,8 +14,11 @@ export const Pages: VFC = () => {
         <Flex w="75%" justifyContent="center">
           <Box w="95%">
             <ChatHeader />
-            <Box>
+            <Box h="75vh" overflow="scroll">
               <Chat />
+            </Box>
+            <Box mt="4">
+              <ChatForm />
             </Box>
           </Box>
         </Flex>
--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -22,9 +22,30 @@ export const Chat: VFC = () => {
         </Flex>
         <Flex justifyContent="flex-end">
           <Box w="43%" bgColor="#56A9D3" color="#fff" p="3" mt="4">
+            <Text>お疲れ様です！ こちら本日分の日誌です。</Text>
+          </Box>
+        </Flex>
+        <Flex justifyContent="flex-end">
+          <Box w="43%" bgColor="#56A9D3" color="#fff" p="3" mt="4">
+            <Text>お疲れ様です！ こちら本日分の日誌です。</Text>
+          </Box>
+        </Flex>
+        <Flex justifyContent="flex-end">
+          <Box w="43%" bgColor="#56A9D3" color="#fff" p="3" mt="4">
+            <Text>お疲れ様です！ こちら本日分の日誌です。</Text>
+          </Box>
+        </Flex>
+        <Flex justifyContent="flex-end">
+          <Box w="43%" bgColor="#56A9D3" color="#fff" p="3" mt="4">
             <Text>また、不明な点がありましたら気軽にお尋ねください！</Text>
           </Box>
         </Flex>
+      </Box>
+      <Box w="43%" bgColor="#E9E9E9" p="3" mt="4">
+        <Text>
+          本日の日誌確認しました。 慣れない現場で大変だったと思います。
+          ゆっくり休んでください！ お疲れ様でした
+        </Text>
       </Box>
       <Box w="43%" bgColor="#E9E9E9" p="3" mt="4">
         <Text>
