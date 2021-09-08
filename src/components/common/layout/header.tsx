d85b878c0e29b7ad12390ffd5a167faed79689b8
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-08 20:55:47 +0900

    feat:プロフィールUIの作成

--- src/components/common/layout/header.tsx
+++ src/components/common/layout/header.tsx
@@ -6,8 +6,14 @@ import {
   Text,
   InputGroup,
   InputRightElement,
+  Menu,
+  Button,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
 import { VFC } from "react";
+import Link from "next/link";
 import { MdSearch } from "react-icons/md";
 
 export const Header: VFC = () => {
@@ -22,17 +28,29 @@ export const Header: VFC = () => {
             </InputRightElement>
           </InputGroup>
         </Box>
-        <Box>
-          <Flex>
-            <Avator />
-            <Box pl={2}>
-              <Text fontSize="md" fontWeight="bold">
-                田中ジョニー
-              </Text>
-              <Text fontSize="sm">22卒</Text>
+        <Menu>
+          <MenuButton as={Button} bg="#fff">
+            <Box>
+              <Flex>
+                <Avator />
+                <Box pl={2}>
+                  <Text fontSize="md" fontWeight="bold">
+                    田中ジョニー
+                  </Text>
+                  <Text fontSize="sm">22卒</Text>
+                </Box>
+              </Flex>
             </Box>
-          </Flex>
-        </Box>
+          </MenuButton>
+          <MenuList>
+            <MenuItem>
+              <Link href="/profile">
+                <a>プロフィール</a>
+              </Link>
+            </MenuItem>
+            <MenuItem>ログアウト</MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
     </Box>
   );
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,9 @@
+import { Pages } from "@/components/profile/Pages";
+import { Layout } from "@/components/common/layout";
+export default function index() {
+  return (
+    <Layout isHeader>
+      <Pages />
+    </Layout>
+  );
+}
