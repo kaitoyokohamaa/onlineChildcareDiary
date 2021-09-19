9dee960ed24a2b22e3a2f8503312e40d4ac3d26f
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-19 14:24:56 +0900

    feat:登録情報の一覧表示

--- src/components/diary/Pages.tsx
+++ src/components/diary/Pages.tsx
@@ -1,43 +1,61 @@
-import { VFC } from "react";
-import { MdLocalLibrary } from "react-icons/md";
-import { Box, Flex, Text, Divider } from "@chakra-ui/layout";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
-import Link from "next/link";
+import {VFC} from 'react'
+import {MdLocalLibrary} from 'react-icons/md'
+import {Box, Flex, Text, Divider} from '@chakra-ui/layout'
+import {Table, Thead, Tbody, Tr, Th, Td} from '@chakra-ui/react'
+import Link from 'next/link'
+import {useEffect, useState} from 'react'
+import {registerRef} from '@/lib/firestore'
 export const Pages: VFC = () => {
-  return (
-    <Box mt="10" px={16}>
-      <Flex alignItems="center">
-        <Box bg="#F8F8F8" p="2" borderRadius="md">
-          <MdLocalLibrary color=" #9FD0E8" />
-        </Box>
-        <Text pl="8" fontWeight="bold">
-          日誌一覧
-        </Text>
-      </Flex>
-      <Divider mt="5" />
-      <Table variant="striped" colorScheme="twitter">
-        <Thead>
-          <Tr>
-            <Th>日付</Th>
-            <Th isNumeric></Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {[...Array(10)].map(() => {
-            return (
-              <Tr
-                _hover={{
-                  boxShadow: "dark-lg",
-                  p: "14",
-                }}
-              >
-                <Td>2021/09/31(第1週目)</Td>
-                <Td isNumeric>詳細</Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </Box>
-  );
-};
+	const [diaries, setDiaries] = useState([])
+	let diariesArray = []
+	useEffect(() => {
+		registerRef().onSnapshot((res) => {
+			res.forEach((item) => {
+				diariesArray.push({diary: item.data(), docID: item.data().id})
+			})
+			setDiaries(diariesArray)
+		})
+	}, [])
+
+	return (
+		<Box mt="10" px={16}>
+			<Flex alignItems="center">
+				<Box bg="#F8F8F8" p="2" borderRadius="md">
+					<MdLocalLibrary color=" #9FD0E8" />
+				</Box>
+				<Text pl="8" fontWeight="bold">
+					日誌一覧
+				</Text>
+			</Flex>
+			<Divider mt="5" />
+			<Table variant="striped" colorScheme="twitter">
+				<Thead>
+					<Tr>
+						<Th>日付</Th>
+						<Th isNumeric></Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					{diaries.length ? (
+						diaries.map((res) => {
+							console.log(res.diary)
+							return (
+								<Tr
+									_hover={{
+										boxShadow: 'dark-lg',
+										p: '14'
+									}}
+								>
+									<Td>{`${res.diary.day} ${res.diary.assignedName}`}組の保育日誌</Td>
+									<Td isNumeric>詳細</Td>
+								</Tr>
+							)
+						})
+					) : (
+						<p>日誌はまだ登録されておりません</p>
+					)}
+				</Tbody>
+			</Table>
+		</Box>
+	)
+}
