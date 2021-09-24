94bd6358a5db2c156a6d117e223e0634e8211fc6
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-24 10:44:05 +0900

    fix:include tscongfig my font type

--- font.d.ts
+++ font.d.ts
@@ -1,1 +1,10 @@
-declare module '*.ttf'
+declare module 'next-fonts'
+declare module '*.ttf' {
+	const content: any
+	export default content
+}
+
+declare module '*.otf' {
+	const content: any
+	export default content
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-declare module 'next-fonts'
-declare module '*.ttf' {
-	const content: any
-	export default content
-}
-
-declare module '*.otf' {
-	const content: any
-	export default content
-}
