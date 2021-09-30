e44f991dc28115508651b3558e0da3f0ba57a11d
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-30 18:26:14 +0900

    fix:強制的にstring型に変更

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -3,7 +3,7 @@ import * as admin from 'firebase-admin'
 if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
-			projectId: process.env.FIREBASE_PROJECT_ID,
+			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 			privateKey: process.env.FIREBASE_PRIVATE_KEY
 		})
