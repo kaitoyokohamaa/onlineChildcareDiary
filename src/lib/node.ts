77cb0ddc48970c7f3866caaad5262be41b9282e4
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-30 18:36:08 +0900

    fix:再度clientemailを型で強制的にstringへ

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,11 +1,10 @@
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
-	console.log(process.env.FIREBASE_PRIVATE_KEY)
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
-			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+			clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
 			privateKey: String(process.env.FIREBASE_PRIVATE_KEY)
 		})
 	})
