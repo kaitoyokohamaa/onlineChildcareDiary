cad0bf219207075c6e11384bc28b66e4e2680271
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-30 19:05:02 +0900

    fix:fix privatekey初期化のエラー6回目

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,11 +1,13 @@
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
+	const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64')
+	const firebase_private_key = firebase_private_key_b64.toString('utf8')
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
-			privateKey: JSON.parse(String(process.env.FIREBASE_PRIVATE_KEY)).replace(/\\n/g, '\n')
+			privateKey: firebase_private_key
 		})
 	})
 }
