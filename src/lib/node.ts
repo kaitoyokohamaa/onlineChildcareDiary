ad4b201c85c7e43aac5ebcd25831c8787dad36fd
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-30 18:30:28 +0900

    fix:再度privateKeyを型で強制的にstringへ

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,11 +1,12 @@
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
+	console.log(process.env.FIREBASE_PRIVATE_KEY)
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.FIREBASE_PRIVATE_KEY
+			privateKey: String(process.env.FIREBASE_PRIVATE_KEY)
 		})
 	})
 }
