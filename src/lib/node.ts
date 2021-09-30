461a17b7b54c671ebfe7a1d41d2da23a3dad7e67
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-09-30 19:07:57 +0900

    fix:fix privatekey初期化のエラー7回目

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
-	const firebase_private_key_b64 = Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64')
+	const firebase_private_key_b64 = Buffer.from(String(process.env.FIREBASE_PRIVATE_KEY), 'base64')
 	const firebase_private_key = firebase_private_key_b64.toString('utf8')
 	admin.initializeApp({
 		credential: admin.credential.cert({
