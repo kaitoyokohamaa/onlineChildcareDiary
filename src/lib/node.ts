c8784e3907b5e4c11b8648afa41e2d6b8579e8e7
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-10-01 13:19:51 +0900

    fix:firebase admin

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,15 +1,11 @@
 import * as admin from 'firebase-admin'
-const dotenv = require('dotenv')
-dotenv.config()
 
 if (!admin.apps.length) {
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
-			privateKey: String(process.env.FIREBASE_PRIVATE_KEY)
-				? String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n')
-				: undefined
+			privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n')
 		})
 	})
 }
