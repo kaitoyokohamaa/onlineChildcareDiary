dd71ab096f5edec5ee32e11bd3f57f3a764d3a54
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-10-01 12:51:17 +0900

    fix:private key again

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -1,13 +1,11 @@
 import * as admin from 'firebase-admin'
 
 if (!admin.apps.length) {
-	const firebase_private_key_b64 = Buffer.from(String(process.env.FIREBASE_PRIVATE_KEY), 'base64')
-	const firebase_private_key = firebase_private_key_b64.toString('utf8')
 	admin.initializeApp({
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
-			privateKey: firebase_private_key
+			privateKey: String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n')
 		})
 	})
 }
