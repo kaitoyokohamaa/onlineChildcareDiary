df3c3d83a06424f81303dfad6720392d5057414c
kaitoyokohamaa <s1A121700885@toyo.jp>
2021-10-01 12:57:15 +0900

    fix:fix private keyのbuild err

--- src/lib/node.ts
+++ src/lib/node.ts
@@ -5,7 +5,9 @@ if (!admin.apps.length) {
 		credential: admin.credential.cert({
 			projectId: String(process.env.FIREBASE_PROJECT_ID),
 			clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
-			privateKey: String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n')
+			privateKey: String(process.env.FIREBASE_PRIVATE_KEY)
+				? String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, '\n')
+				: undefined
 		})
 	})
 }
