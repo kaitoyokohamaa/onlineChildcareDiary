9493c1d065265ee9f70afabc32ba4da0ba67bcf6
yoko <s1A121700885@toyo.jp>
2021-10-04 17:29:39 +0900

    feat:profile 修正

--- src/components/profile/Pages.tsx
+++ src/components/profile/Pages.tsx
@@ -5,146 +5,133 @@ import {Avatar, useColorModeValue, Button, Divider} from '@chakra-ui/react'
 import {useRouter} from 'next/router'
 import Link from 'next/link'
 export const Pages: VFC<{user: User}> = ({user}) => {
-	const router = useRouter()
-	const currentPath = router.asPath
-	const editPath = currentPath.replace('profile', 'edit')
-	return (
-		<Box mt="10" px={16} overflow="scroll" h="85vh">
-			<Text
-				fontWeight="bold"
-				w="100%"
-				fontSize="18px"
-				position="relative"
-				display="inline-block"
-				_after={{
-					position: 'absolute',
-					top: '50%',
-					display: 'inline-block',
-					width: '87%',
-					height: '1px',
-					left: '13%',
-					backgroundColor: '#00000029',
-					content: '" "'
-				}}
-			>
-				プロフィール
-			</Text>
+  const router = useRouter()
+  const currentPath = router.asPath
+  const editPath = currentPath.replace('profile', 'edit')
+  return (
+    <Box mt="10" px={16} overflow="scroll" h="85vh">
+      <Text
+        fontWeight="bold"
+        w="100%"
+        fontSize="18px"
+        position="relative"
+        display="inline-block"
+        _after={{
+          position: 'absolute',
+          top: '50%',
+          display: 'inline-block',
+          width: '87%',
+          height: '1px',
+          left: '13%',
+          backgroundColor: '#00000029',
+          content: '" "'
+        }}
+      >
+        プロフィール
+      </Text>
 
-			<Center py={6}>
-				<Box
-					w="100%"
-					bg={useColorModeValue('white', 'gray.900')}
-					boxShadow={'2xl'}
-					rounded={'2xl'}
-					overflow={'hidden'}
-					p="14"
-				>
-					<Flex>
-						<Box>
-							<Avatar size="2xl" src={user.dispayImage} alt={'Author'} />
-						</Box>
+      <Flex py={1}>
+        <Box w="100%" boxShadow={'2xl'} p="14" my="10">
+          <Flex>
+            <Box mr="10">
+              <Avatar size="2xl" src={user.dispayImage} alt={'Author'} />
+            </Box>
 
-						<Box pl="5">
-							<Heading
-								color={useColorModeValue('gray.700', 'white')}
-								fontSize={'2xl'}
-								fontWeight="bold"
-								fontFamily={'body'}
-							>
-								{user.name ? user.name : '未記入'}
-							</Heading>
-							<Text color={'gray.500'} pt="5">
-								{user.selfIntroduction ? user.selfIntroduction : '未記入'}
-							</Text>
-						</Box>
-					</Flex>
-				</Box>
-			</Center>
-			<Box
-				w="100%"
-				bg={useColorModeValue('white', 'gray.900')}
-				boxShadow={'2xl'}
-				rounded={'2xl'}
-				overflow={'hidden'}
-				p="14"
-				my="10"
-			>
-				<Flex>
-					<Box>
-						<Heading
-							color={useColorModeValue('gray.700', 'white')}
-							fontSize={'2xl'}
-							fontWeight="bold"
-							fontFamily={'body'}
-							mb="10"
-						>
-							基本情報
-						</Heading>
-					</Box>
-				</Flex>
-				<Flex>
-					{/* 大学名 */}
-					<Box w="18%">
-						<Text fontWeight="bold" color="#273264">
-							大学
-						</Text>
-						<Text pt="2">東洋大学</Text>
-					</Box>
-					{/* 性別 */}
-					<Box w="18%">
-						<Text fontWeight="bold" color="#273264">
-							性別
-						</Text>
-						<Text pt="2">{user.sex ? user.sex : '未記入'}</Text>
-					</Box>
-					{/* 生年月日 */}
-					<Box w="18%">
-						<Text fontWeight="bold" color="#273264">
-							生年月日
-						</Text>
-						<Text pt="2">{user.birthday ? user.birthday : '未記入'}</Text>
-					</Box>
-					{/* メールアドレス */}
-					<Box>
-						<Text fontWeight="bold" color="#273264">
-							メールアドレス
-						</Text>
-						<Text pt="2">{user.address ? user.address : '未記入'}</Text>
-					</Box>
-				</Flex>
-				<Divider mt="10" colorScheme="blue" size="2xl" />
-				<Flex pt="10">
-					{/* 電話アドレス */}
-					<Box w="18%">
-						<Text fontWeight="bold" color="#273264">
-							電話
-						</Text>
-						<Text pt="2">{user.cellphoneNumber ? user.cellphoneNumber : '未記入'}</Text>
-					</Box>
+            <Box pl="5">
+              <Flex justifyContent="space-between">
+                <Heading
+                  color={useColorModeValue('gray.700', 'white')}
+                  fontSize={'2xl'}
+                  fontWeight="bold"
+                  fontFamily={'body'}
+                >
+                  {user.name ? user.name : '未記入'}
+                </Heading>
+                <Box mt="-4">
+                  <Button
+                    background="#263773"
+                    color="#fff"
+                    _hover={{background: '#1c2956'}}
+                  >
+                    <Link href={`/profile${editPath}`}>
+                      <a>プロフィールを編集する</a>
+                    </Link>
+                  </Button>
+                </Box>
+              </Flex>
+              <Text color={'gray.500'} pt="5">
+                {user.selfIntroduction ? user.selfIntroduction : '未記入'}
+              </Text>
+            </Box>
+          </Flex>
 
-					{/* 実習先*/}
-					<Box>
-						<Text fontWeight="bold" color="#273264">
-							実習先
-						</Text>
-						<Text pt="2">{user.practicalTraining ? user.practicalTraining : '未記入'}</Text>
-					</Box>
-				</Flex>
-			</Box>
-			<Box textAlign="end">
-				<Button
-					background="#263773"
-					color="#fff"
-					_hover={{background: '#1c2956'}}
-					textAlign="right"
-					my="1"
-					mb="10"
-				>
-					<Link href={`/profile${editPath}`}>
-						<a>プロフィールを編集する</a>
-					</Link>
-				</Button>
-			</Box>
-		</Box>
-	)
+          <Flex mt="10">
+            <Box>
+              <Heading
+                color={useColorModeValue('gray.700', 'white')}
+                fontSize={'2xl'}
+                fontWeight="bold"
+                fontFamily={'body'}
+                mb="10"
+              >
+                基本情報
+              </Heading>
+            </Box>
+          </Flex>
+          <Flex>
+            {/* 大学名 */}
+            <Box w="25%">
+              <Text fontWeight="bold" color="#273264">
+                大学
+              </Text>
+              <Text pt="2">東洋大学</Text>
+            </Box>
+            {/* 性別 */}
+            <Box w="25%">
+              <Text fontWeight="bold" color="#273264">
+                性別
+              </Text>
+              <Text pt="2">{user.sex ? user.sex : '未記入'}</Text>
+            </Box>
+            {/* 生年月日 */}
+            <Box w="25%">
+              <Text fontWeight="bold" color="#273264">
+                生年月日
+              </Text>
+              <Text pt="2">{user.birthday ? user.birthday : '未記入'}</Text>
+            </Box>
+            {/* メールアドレス */}
+            <Box>
+              <Text fontWeight="bold" color="#273264">
+                メールアドレス
+              </Text>
+              <Text pt="2">{user.address ? user.address : '未記入'}</Text>
+            </Box>
+          </Flex>
+          <Divider mt="10" colorScheme="blue" size="2xl" />
+          <Flex py="10">
+            {/* 電話アドレス */}
+            <Box w="25%">
+              <Text fontWeight="bold" color="#273264">
+                電話
+              </Text>
+              <Text pt="2">
+                {user.cellphoneNumber ? user.cellphoneNumber : '未記入'}
+              </Text>
+            </Box>
+
+            {/* 実習先*/}
+            <Box>
+              <Text fontWeight="bold" color="#273264">
+                実習先
+              </Text>
+              <Text pt="2">
+                {user.practicalTraining ? user.practicalTraining : '未記入'}
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
 }
