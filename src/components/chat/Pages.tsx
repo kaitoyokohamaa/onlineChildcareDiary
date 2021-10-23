80584220423c081d6bafd29486aac78f8196a5da
yoko <s1A121700885@toyo.jp>
2021-10-23 20:45:35 +0900

    型の追加

--- src/components/chat/Pages.tsx
+++ src/components/chat/Pages.tsx
@@ -1,12 +1,33 @@
-import {VFC} from 'react'
+import {VFC, useState, useEffect} from 'react'
 import {Box, Flex} from '@chakra-ui/layout'
-
+import {chatRef} from '@/lib/firestore'
 import {ChatSidebar} from '@/components/chat/chatSidebar'
 import {ChatHeader} from '@/components/chat/chatHeader'
 import {Chat} from '@/components/chat/chat'
 import {ChatForm} from '@/components/chat/chatForm'
 import {Layout} from '@/components/common/layout'
+import {Messages} from '@/models/chat'
 export const Pages: VFC<{chatKey: string}> = ({chatKey}) => {
+  const [chatMessages, setChatMessages] = useState<Messages[]>([])
+
+  useEffect(() => {
+    chatRef(chatKey)
+      .orderBy('sentAt', 'asc')
+      .onSnapshot((res) => {
+        let chatMessagesArray: Messages[] = []
+        res.forEach((item) => {
+          chatMessagesArray.push({
+            chatsId: item.id,
+            chats: {
+              senderId: item.data().senderId,
+              sentAt: item.data().sentAt,
+              text: item.data().text
+            }
+          })
+        })
+        setChatMessages(chatMessagesArray)
+      })
+  }, [chatKey])
   return (
     <Layout isHeader>
       <Box p={0} borderTop="2px" borderColor="#E9E9E9" mt="20px">
@@ -16,7 +37,7 @@ export const Pages: VFC<{chatKey: string}> = ({chatKey}) => {
             <Box w="95%">
               <ChatHeader />
               <Box h="65vh" overflow="scroll">
-                <Chat chatKey={chatKey} />
+                <Chat chatMessages={chatMessages} />
               </Box>
               <Box mt="4">
                 <ChatForm />
--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,39 +1,17 @@
-import {VFC, useState, useEffect} from 'react'
+import {VFC} from 'react'
 
 import {ChatText} from '@/components/chat/chatText'
-import {chatRef} from '@/lib/firestore'
-export const Chat: VFC<{chatKey: string}> = ({chatKey}) => {
-  const [chatMessages, setChatMessages] = useState()
 
-  useEffect(() => {
-    chatRef(chatKey)
-      .orderBy('sentAt', 'asc')
-      .onSnapshot((res) => {
-        let chatMessagesArray = []
-        res.forEach((item) => {
-          chatMessagesArray.push({chatsId: item.id, chats: item.data()})
-        })
-        setChatMessages(chatMessagesArray)
-      })
-  }, [setChatMessages, chatKey])
+import {Messages} from '@/models/chat'
+
+export const Chat: VFC<{chatMessages: Messages[]}> = ({chatMessages}) => {
   return (
     <>
-      {chatMessages?.map((res) => {
+      {chatMessages?.map((res: Messages) => {
         return (
-          <ChatText
-            key={res.chatsId.id}
-            isTeacher={false}
-            text={res.chats.text}
-          />
+          <ChatText key={res.chatsId} isTeacher={false} text={res.chats.text} />
         )
       })}
-      <ChatText
-        isTeacher={true}
-        text={`ありがとう
-      ore
-      多fkrforこ
-      トンカツ`}
-      />
     </>
   )
 }
