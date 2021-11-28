8251a47ab40c731e296b696a4f1f2ffdbf3d5062
s1a121700885@toyo.jp <s1A121700885@toyo.jp>
2021-11-28 11:56:27 +0900

    fix:diary history fix again

--- src/pages/teacher/history/[[...history]].tsx
+++ src/pages/teacher/history/[[...history]].tsx
@@ -5,8 +5,8 @@ import {History} from '@/models/diary/history';
 export const getStaticProps: GetStaticProps = async (
   context: GetStaticPropsContext,
 ) => {
-  const historyKey = context.params.history[1];
-  const userKey = context.params.history[2];
+  const historyKey = context.params.history[0];
+  const userKey = context.params.history[1];
   console.log(historyKey);
   const history = await teacherRef()
     .doc(String(historyKey))
